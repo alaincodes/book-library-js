e0fdb608eb6f2b2f1b6596f395a4f247d72912af
Alain <alaint77@gmail.com>
2019-06-30 00:01:41 +0200

    update js logic..

--- src/app.js
+++ src/app.js
@@ -2,11 +2,35 @@ const modal = document.getElementById("modal-box");
 const modalButton = document.getElementById("modal-btn");
 const closeModalBtn = document.getElementsByClassName("close-modal")[0];
 
-let myLibrary = [];
+const title = document.getElementById("title").value;
+const author = document.getElementById("author").value;
+const pages = document.getElementById("pages").value;
+const read = document.getElementById("read").value;
+let read = false;
 
-function Book() {}
+let myLibrary = [
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 254,
+    read: false
+  }
+];
 
-function addBookToLibrary() {}
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary(event) {}
+
+function render() {}
+
+function deleteBook() {}
 
 modalButton.addEventListener("click", () => {
   modal.style.display = "block";
